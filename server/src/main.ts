287cab65698e5f1f697c17d7194f42149ad2a711
anton_shev <ashevtsov.@webpractik.ru>
2022-01-22 18:46:28 +0300

    feat(back): установка swagger

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Mamblish app')
+    .setDescription('The Mamblish App API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(5000);
 }
-bootstrap();
+
+bootstrap().catch((e) => console.log(e));
