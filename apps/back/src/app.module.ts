0ead17af2f3ed05b9d71f71efba9bd8f9123b2ec
mamblijoe <YtL6xgfr7sxduA>
2022-07-17 18:51:57 +0300

    feat(nginx): удалил лишние модули с бека

--- apps/back/src/app.module.ts
+++ apps/back/src/app.module.ts
@@ -1,24 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { NotificationModule } from './notification/notification.module';
-import { NotificationGateway } from './notification/notification.gateway';
 import { ScheduleModule } from '@nestjs/schedule';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'db',
-    port: 3306,
-    username: 'db',
-    password: 'db',
-    database: 'db',
-    entities: ['dist/**/*.entity{.ts,.js}'],
-    logging: true,
-    synchronize: true,
-  }),NotificationModule, ScheduleModule.forRoot(), TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'db',
+      port: 3306,
+      username: 'db',
+      password: 'db',
+      database: 'db',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      logging: true,
+      synchronize: true,
+    }),
+    ScheduleModule.forRoot(),
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
