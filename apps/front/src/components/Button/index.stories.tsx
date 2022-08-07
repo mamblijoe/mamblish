6f1d9dbbc4d2c53310e702f907deadcec176de76
mamblijoe <YtL6xgfr7sxduA>
2022-08-07 22:19:56 +0300

    feat(front): настройка storybook

--- apps/front/src/components/Button/index.stories.tsx
+++ apps/front/src/components/Button/index.stories.tsx
@@ -4,12 +4,32 @@ import React from 'react';
 import Button from '@/components/Button';
 
 export default {
-    title: 'Example/Index',
+    title: 'Button',
     component: Button,
     argTypes: {
         backgroundColor: { control: 'color' },
         primary: { control: 'boolean' },
+        size: {
+            control: 'select',
+            options: ['large', 'medium', 'small'],
+        },
     },
+    parameters: {
+        backgrounds: {
+            values: [
+                { name: 'red', value: '#f00' },
+                { name: 'green', value: '#0f0' },
+                { name: 'blue', value: '#00f' },
+            ],
+        },
+    },
+    decorators: [
+        Story => (
+            <div style={{ border: '1px solid red', padding: '5px' }}>
+                <Story />
+            </div>
+        ),
+    ],
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
@@ -17,22 +37,28 @@ const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
     primary: true,
-    label: 'Button',
+    label: 'Primary Button',
 };
 
 export const Secondary = Template.bind({});
 Secondary.args = {
-    label: 'Button',
+    label: 'Secondary Button',
 };
 
 export const Large = Template.bind({});
 Large.args = {
     size: 'large',
-    label: 'Button',
+    label: 'Large Button',
 };
 
 export const Small = Template.bind({});
 Small.args = {
     size: 'small',
-    label: 'Button',
+    label: 'Small Button',
+};
+
+export const Medium = Template.bind({});
+Medium.args = {
+    size: 'medium',
+    label: 'Medium Button',
 };
